8c2abc54886895deabbf97bf0592a740e552da8a
MLSALEX <stiopovna@mail.ru>
2023-09-27 17:36:29 +0300

    HW nr 9

---
+++ src/main/java/homework_nr_9/Herbivore.java
@@ -0,0 +1,9 @@
+package homework_nr_9;
+
+public abstract class Herbivore {
+    public abstract void eatPlants();
+
+    public void eatFruits() {
+        System.out.println("Some herbivores eat fruits");
+    }
+}
