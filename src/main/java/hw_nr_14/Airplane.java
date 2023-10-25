e009bf2b5c1044187b0d1a2d223e5d490d9b0394
MLSALEX <stiopovna@mail.ru>
2023-10-25 11:46:10 +0300

    hw 13

---
+++ src/main/java/hw_nr_14/Airplane.java
@@ -0,0 +1,8 @@
+package hw_nr_14;
+
+public class Airplane extends Vehicle implements Flyable{
+    @Override
+    public void CanFly() {
+
+    }
+}
---
+++ src/main/java/hw_nr_14/Helicopter.java
@@ -0,0 +1,8 @@
+package hw_nr_14;
+
+public class Helicopter extends Vehicle implements Flyable {
+    @Override
+    public void CanFly() {
+
+    }
+}
